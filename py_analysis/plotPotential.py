b8d03fa5494c1b5dd5745c4f22a87b8635c442ec
ctorney <colin.j.torney@gmail.com>
2014-06-27 11:48:02 -0400

    included correlation coefficient with 2 stages

--- py_analysis/plotPotential.py
+++ py_analysis/plotPotential.py
@@ -1,62 +1,73 @@
-"""
-Broken axis example, where the y-axis will have a portion cut out.
-"""
-import matplotlib.pylab as plt
+
+#!/usr/bin/python
+
+import sympy as sp
+from IPython.display import display
 import numpy as np
+import matplotlib.pyplot as plt
+import math
+import matplotlib as mpl
+
+K = 8        
+wg = 0.2875
+ws = 0.65
+alpha = 1
 
 
-# 30 points between 0 0.2] originally made using np.random.rand(30)*.2
-pts = np.array([ 0.015,  0.166,  0.133,  0.159,  0.041,  0.024,  0.195,
-    0.039, 0.161,  0.018,  0.143,  0.056,  0.125,  0.096,  0.094, 0.051,
-    0.043,  0.021,  0.138,  0.075,  0.109,  0.195,  0.05 , 0.074, 0.079,
-    0.155,  0.02 ,  0.01 ,  0.061,  0.008])
 
-# Now let's make two outlier points which are far away from everything.
-pts[[3,14]] += .8
+for acount in range(0,11):
+    alpha = 0.1*float(acount)
+    
 
-# If we were to simply plot pts, we'd lose most of the interesting
-# details due to the outliers. So let's 'break' or 'cut-out' the y-axis
-# into two portions - use the top (ax) for the outliers, and the bottom
-# (ax2) for the details of the majority of our data
-f,(ax,ax2) = plt.subplots(2,1,sharex=True)
+    sampleP = np.load('potential' + '{0:02d}'.format(int(10.0*alpha)) + '.npy')
+    xGrid=np.arange(65)/64.0
+    
+    
+    pot=np.log(np.divide(sampleP[1,:],sampleP[0,:]))
+    pot=np.cumsum(pot[2:64])
+    plt.plot(xGrid[2:64],pot)
+    potmatch = pot[0]
+    
+    plt.axis([0, 1, -10, 10])
+    
 
-# plot the same data on both axes
-ax.plot(pts)
-ax2.plot(pts)
+def tup( x ):
+    alpha1 = alpha*1.0/(1.0+exp(-(x-xthresh)/thresh))
+    if x > 0.0625:
+        alpha1=alpha
+    else:
+        alpha1 = 0
+    alpha1 = alpha*(1.0-exp(-(x)/0.1))
+    xx = (1.0-alpha1)*(x)
+    return (1-x) * sum(sp.binomial(K,j) * xx**j * (1-xx)**(K-j) * psw(j) for j in xrange(0,K+1))
 
-# zoom-in / limit the view to different portions of the data
-ax.set_ylim(.78,1.) # outliers only
-ax2.set_ylim(0,.22) # most of the data
+def tdown( x ):
+    alpha1 = alpha*1.0/(1.0+exp(-(x-xthresh)/thresh))
+    if x > 0.0625:
+        alpha1=alpha
+    else:
+        alpha1 = 0
+    alpha1 = alpha*(1.0-exp(-(x)/0.1))        
+    xx = 1.0-(1.0-alpha1)*(1.0-x)
+    return (x) * (1 -  sum(sp.binomial(K,j) * xx**j * (1-xx)**(K-j) * psw(j) for j in xrange(0,K+1)))
 
-# hide the spines between ax and ax2
-ax.spines['bottom'].set_visible(False)
-ax2.spines['top'].set_visible(False)
-ax.xaxis.tick_top()
-ax.tick_params(labeltop='off') # don't put tick labels at the top
-ax2.xaxis.tick_bottom()
 
-# This looks pretty good, and was fairly painless, but you can get that
-# cut-out diagonal lines look with just a bit more work. The important
-# thing to know here is that in axes coordinates, which are always
-# between 0-1, spine endpoints are at these locations (0,0), (0,1),
-# (1,0), and (1,1).  Thus, we just need to put the diagonals in the
-# appropriate corners of each of our axes, and so long as we use the
-# right transform and disable clipping.
+figure()
+for acount in range(0,6):
+    alpha = 0.15*float(acount)
+    
+    for x in range(0,65):
+        thGridup[x] = tup(x/64.0)
+        thGriddown[x] = tdown(x/64.0)
 
-d = .015 # how big to make the diagonal lines in axes coordinates
-# arguments to pass plot, just so we don't keep repeating them
-kwargs = dict(transform=ax.transAxes, color='k', clip_on=False)
-ax.plot((-d,+d),(-d,+d), **kwargs)      # top-left diagonal
-ax.plot((1-d,1+d),(-d,+d), **kwargs)    # top-right diagonal
-
-kwargs.update(transform=ax2.transAxes)  # switch to the bottom axes
-ax2.plot((-d,+d),(1-d,1+d), **kwargs)   # bottom-left diagonal
-ax2.plot((1-d,1+d),(1-d,1+d), **kwargs) # bottom-right diagonal
-
-# What's cool about this is that now if we vary the distance between
-# ax and ax2 via f.subplots_adjust(hspace=...) or plt.subplot_tool(),
-# the diagonal lines will move accordingly, and stay right at the tips
-# of the spines they are 'breaking'
-
-plt.show()# -*- coding: utf-8 -*-
+    xGrid=np.arange(65)/64.0
+    
+    
+    pot=np.log(np.divide(sampleP[1,:],sampleP[0,:]))
+    pot=np.log(np.divide(thGriddown,thGridup))
+    pot=np.cumsum(pot[2:64])
+    plt.plot(xGrid[2:64],pot)
+    potmatch = pot[0]
+    
+    plt.axis([0, 1, -10, 10])
 
--- py_analysis/transProb.py
+++ py_analysis/transProb.py
@@ -11,8 +11,10 @@ import matplotlib as mpl
 K = 8        
 wg = 0.2875
 ws = 0.65
-alpha = 0.4
-
+alpha = 0.00000450
+thresh = 0.125
+xthresh = 0.06125
+NA = 64
 def psw( j ):
     gc = np.log(ws/(1-ws))*(K-2*j)/(4*wg)
     return 0.5 + 0.5*math.erf(math.sqrt(wg)*(1.0-gc))
@@ -20,11 +22,23 @@ def psw( j ):
 
 # Function definition is here
 def tup( x ):
-    xx = (1.0-alpha)*(x)
+    alpha1 = alpha*1.0/(1.0+exp(-(x-xthresh)/thresh))
+    if x > 0.0625:
+        alpha1=alpha
+    else:
+        alpha1 = 0
+    alpha1 = alpha*(1.0-exp(-x/0.1))
+    xx = (1.0-alpha1)*(x)
     return (1-x) * sum(sp.binomial(K,j) * xx**j * (1-xx)**(K-j) * psw(j) for j in xrange(0,K+1))
 
 def tdown( x ):
-    xx = 1.0-(1.0-alpha)*(1.0-x)
+    alpha1 = alpha*1.0/(1.0+exp(-(x-xthresh)/thresh))
+    if x > 0.0625:
+        alpha1=alpha
+    else:
+        alpha1 = 0
+    alpha1 = alpha*(1.0-exp(-x/0.1))        
+    xx = 1.0-(1.0-alpha1)*(1.0-x)
     return (x) * (1 -  sum(sp.binomial(K,j) * xx**j * (1-xx)**(K-j) * psw(j) for j in xrange(0,K+1)))
 
 
@@ -55,19 +69,22 @@ thGriddown=np.zeros(65)
 for x in range(0,65):
     thGridup[x] = tup(x/64.0)
     thGriddown[x] = tdown(x/64.0)
-#plt.plot(xGrid,thGridup,label='theory up')
-#plt.plot(xGrid,thGriddown,label='theory down')
+plt.plot(xGrid,thGridup,label='theory up')
+plt.plot(xGrid,thGriddown,label='theory down')
 legend()
 plt.figure()
 
+sampleP = np.load('potential00.npy')
 pot=np.log(np.divide(sampleP[1,:],sampleP[0,:]))
 pot=np.cumsum(pot[2:64])
 plt.plot(xGrid[2:64],pot)
-potmatch = pot[32]
+
+potmatch = pot[0]
+#pot=np.log(np.divide(sampleP[1,:],sampleP[0,:]))
 pot=np.log(np.divide(thGriddown,thGridup))
 pot=np.cumsum(pot[2:64])
-potdiff = potmatch-pot[32]
-#pot = pot + potdiff
+potdiff = potmatch-pot[0]
+pot = pot + potdiff
 plt.plot(xGrid[2:64],pot)
 
-
+plt.axis([0, 1, -4, 10])
